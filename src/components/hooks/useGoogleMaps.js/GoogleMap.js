304223e260d5ce400814df26cac7c1c5e2ef09f7
Michael Jakobsen <michael@rpair.co.uk>
2022-05-07 23:42:53 +0100

    removed test API key and replaced with variable to ensure secure API key, the old API key has been terminated

--- src/components/hooks/useGoogleMaps.js/GoogleMap.js
+++ src/components/hooks/useGoogleMaps.js/GoogleMap.js
@@ -5,9 +5,11 @@ const MapContainer = () => {
   const [currentPosition, setCurrentPosition] = useState({});
 
   const success = (position) => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
     const currentPosition = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude,
+      lat: latitude,
+      lng: longitude,
     };
     setCurrentPosition(currentPosition);
   };
@@ -21,57 +23,52 @@ const MapContainer = () => {
     width: "100%",
   };
 
-  const defaultCenter = {
-    lat: 41.3851,
-    lng: 2.1734,
-  };
-
-  const locations = [
-    {
-      name: "Location 1",
-      location: {
-        lat: 41.3954,
-        lng: 2.162,
-      },
-    },
-    {
-      name: "Location 2",
-      location: {
-        lat: 41.3917,
-        lng: 2.1649,
-      },
-    },
-    {
-      name: "Location 3",
-      location: {
-        lat: 41.3773,
-        lng: 2.1585,
-      },
-    },
-    {
-      name: "Location 4",
-      location: {
-        lat: 41.3797,
-        lng: 2.1682,
-      },
-    },
-    {
-      name: "Location 5",
-      location: {
-        lat: 41.4055,
-        lng: 2.1915,
-      },
-    },
-  ];
+  // const locations = [
+  //   {
+  //     name: "Location 1",
+  //     location: {
+  //       lat: 41.3954,
+  //       lng: 2.162,
+  //     },
+  //   },
+  //   {
+  //     name: "Location 2",
+  //     location: {
+  //       lat: 41.3917,
+  //       lng: 2.1649,
+  //     },
+  //   },
+  //   {
+  //     name: "Location 3",
+  //     location: {
+  //       lat: 41.3773,
+  //       lng: 2.1585,
+  //     },
+  //   },
+  //   {
+  //     name: "Location 4",
+  //     location: {
+  //       lat: 41.3797,
+  //       lng: 2.1682,
+  //     },
+  //   },
+  //   {
+  //     name: "Location 5",
+  //     location: {
+  //       lat: 41.4055,
+  //       lng: 2.1915,
+  //     },
+  //   },
+  // ];
 
   return (
-    <LoadScript googleMapsApiKey="AIzaSyApT3-s4lJ7-Q7Q-fcvUIOkJs_zG5PeXq0">
+    <LoadScript googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY}>
       <GoogleMap
         mapContainerStyle={mapStyles}
         zoom={13}
         center={currentPosition}
       >
-        <Marker position={currentPosition} />
+        {currentPosition.lat && <Marker position={currentPosition} />}
       </GoogleMap>
     </LoadScript>
   );
